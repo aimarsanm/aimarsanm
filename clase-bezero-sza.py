cd83cbcb32de0fcced3b8f7b0beade35fe39965b
aimarsanm <146017584+aimarsanm@users.noreply.github.com>
2023-11-07 14:11:23 +0100

    Actualizar

---
+++ clase-bezero-sza.py
@@ -0,0 +1,20 @@
+Class Bezero:
+ def __init__ (self,bez,password):
+  self.bez=bez
+  self.password=password
+
+Clase Main:
+
+ b1=Bezero(admin,admin)
+ b2=Bezero(pira,pira)
+ b3=Bezero(Miriam,miriam)
+ b4=Bezero(zabi,zabi)
+ b5=Bezero (aimar,aimar)
+
+ BezeroList=[]
+
+ BezerList.append(b1)
+ BezerList.append(b2)
+ BezerList.append(b3)
+ BezerList.append(b4)
+ BezerList.append(b5)
